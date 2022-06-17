39885a34c885f22e6039151c1fccdc981f50de3f
Joeymg08 <Joeymg08@aol.com>
2022-06-17 14:50:51 -0400

    Overhaul, pages

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var left = (screen.width - 1050) / 2;
+var top = (screen.height - 550) / 2;
+
+
+var Bench_Table = "https://montclair225.autodesk360.com/shares/public/SH35dfcQT936092f0e43eeaf688337182486?mode=embed"
+var Metal_Chair = "https://montclair225.autodesk360.com/g/shares/SH35dfcQT936092f0e43da228a37809cc03d?mode=embed";
+var Dough_Mixer = "https://montclair225.autodesk360.com/g/shares/SH35dfcQT936092f0e43e8e02ae73ccad9f9?mode=embed";
+var Knife_Handle = "https://montclair225.autodesk360.com/g/shares/SH35dfcQT936092f0e434b0caddc08fb620a?mode=embed";
+var Drive_Pulley = "https://montclair225.autodesk360.com/g/shares/SH35dfcQT936092f0e43fd87fc9973054022?mode=embed";
