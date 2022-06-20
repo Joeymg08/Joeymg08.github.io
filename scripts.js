44cb4023ad2ad1ed9fb410dabde6c5a162a0c2b7
Joeymg08 <Joeymg08@aol.com>
2022-06-20 14:47:05 -0400

    added gallery

--- scripts.js
+++ scripts.js
@@ -7,3 +7,33 @@ var Metal_Chair = "https://montclair225.autodesk360.com/g/shares/SH35dfcQT936092
 var Dough_Mixer = "https://montclair225.autodesk360.com/g/shares/SH35dfcQT936092f0e43e8e02ae73ccad9f9?mode=embed";
 var Knife_Handle = "https://montclair225.autodesk360.com/g/shares/SH35dfcQT936092f0e434b0caddc08fb620a?mode=embed";
 var Drive_Pulley = "https://montclair225.autodesk360.com/g/shares/SH35dfcQT936092f0e43fd87fc9973054022?mode=embed";
+
+
+
+// image gallery
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" activeDot", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " activeDot";
+}
