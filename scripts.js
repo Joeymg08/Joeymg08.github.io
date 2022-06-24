3759f16baff8a922840e4f7716ed1fdb67a4ef64
Joeymg08 <Joeymg08@aol.com>
2022-06-23 21:57:53 -0400

    major changes

--- scripts.js
+++ scripts.js
@@ -1,14 +1,27 @@
-var left = (screen.width - 1050) / 2;
-var top = (screen.height - 550) / 2;
+var Bench_Table = '<iframe src="https://montclair225.autodesk360.com/shares/public/SH35dfcQT936092f0e43eeaf688337182486?mode=embed" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" frameborder="0"></iframe>';
+var Metal_Chair = '<iframe src="https://montclair225.autodesk360.com/g/shares/SH35dfcQT936092f0e43da228a37809cc03d?mode=embed" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" frameborder="0"></iframe>';
+var Dough_Mixer = '<iframe src="https://montclair225.autodesk360.com/g/shares/SH35dfcQT936092f0e43e8e02ae73ccad9f9?mode=embed" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" frameborder="0"></iframe>';
+var Knife_Handle = '<iframe src="https://montclair225.autodesk360.com/g/shares/SH35dfcQT936092f0e434b0caddc08fb620a?mode=embed" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" frameborder="0"></iframe>';
+var Drive_Pulley = '<iframe src="https://montclair225.autodesk360.com/g/shares/SH35dfcQT936092f0e43fd87fc9973054022?mode=embed" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" frameborder="0"></iframe>';
 
 
-var Bench_Table = "https://montclair225.autodesk360.com/shares/public/SH35dfcQT936092f0e43eeaf688337182486?mode=embed"
-var Metal_Chair = "https://montclair225.autodesk360.com/g/shares/SH35dfcQT936092f0e43da228a37809cc03d?mode=embed";
-var Dough_Mixer = "https://montclair225.autodesk360.com/g/shares/SH35dfcQT936092f0e43e8e02ae73ccad9f9?mode=embed";
-var Knife_Handle = "https://montclair225.autodesk360.com/g/shares/SH35dfcQT936092f0e434b0caddc08fb620a?mode=embed";
-var Drive_Pulley = "https://montclair225.autodesk360.com/g/shares/SH35dfcQT936092f0e43fd87fc9973054022?mode=embed";
+fullscreenContent = document.getElementById("fullscreen");
 
-var bench = '<iframe src="https://montclair225.autodesk360.com/shares/public/SH35dfcQT936092f0e43eeaf688337182486?mode=embed" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" frameborder="0"></iframe>' 
+function openFullscreen(content) {
+  fullscreenContent.innerHTML += content;
+  fullscreenContent.style.display = "block";
+}
+
+function closeFullscreen() {
+  fullscreenContent.innerHTML = '<div id="exit" onclick="closeFullscreen()">Exit</div>'
+  fullscreenContent.style.display = "none";
+}
+
+document.addEventListener('keydown', evt => {
+  if (evt.key === 'Escape') {
+    closeFullscreen();
+  }
+});
 
 // image gallery
 let slideIndex = 1;
@@ -26,33 +39,23 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" activeDot", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " activeDot";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " activeDot";
 }
 
-// function openFullScreen(content){
-//   fullscreen = document.getElementById("fullscreen");
-//   fullscreen.replaceChild(content,content);
-//   fullscreen = "block";
-// }
+// mobilenav
+document.querySelector( ".arrowContainer" ).click(function() {
+  document.querySelector(this).classList.toggle("leftBarAfter");
+});
 
-fullscreenContent = document.getElementById("fullscreen");
-
-function openFullscreen(content) {
-  fullscreenContent.innerHTML += content;
-  fullscreenContent.style.display = "block";
-}
-
-function closeFullscreen(){
-  fullscreenContent.innerHTML = '<div id="exit" onclick="closeFullscreen()">Exit</div>'
-  fullscreenContent.style.display = "none";
-}
-
+document.querySelector( ".arrowContainer" ).click(function() {
+  document.querySelector(this).classList.toggle("rightBarAfter");
+});
