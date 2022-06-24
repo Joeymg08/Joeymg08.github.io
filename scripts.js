c15be681c92341cf7fcb3baf73892f6e8dfac866
Joeymg08 <Joeymg08@aol.com>
2022-06-24 19:28:31 -0400

    major

--- scripts.js
+++ scripts.js
@@ -1,16 +1,19 @@
-var Bench_Table = '<iframe src="https://montclair225.autodesk360.com/shares/public/SH35dfcQT936092f0e43eeaf688337182486?mode=embed" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" frameborder="0"></iframe>';
-var Metal_Chair = '<iframe src="https://montclair225.autodesk360.com/g/shares/SH35dfcQT936092f0e43da228a37809cc03d?mode=embed" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" frameborder="0"></iframe>';
-var Dough_Mixer = '<iframe src="https://montclair225.autodesk360.com/g/shares/SH35dfcQT936092f0e43e8e02ae73ccad9f9?mode=embed" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" frameborder="0"></iframe>';
-var Knife_Handle = '<iframe src="https://montclair225.autodesk360.com/g/shares/SH35dfcQT936092f0e434b0caddc08fb620a?mode=embed" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" frameborder="0"></iframe>';
-var Drive_Pulley = '<iframe src="https://montclair225.autodesk360.com/g/shares/SH35dfcQT936092f0e43fd87fc9973054022?mode=embed" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" frameborder="0"></iframe>';
+var Bench_Table = 'https://montclair225.autodesk360.com/shares/public/SH35dfcQT936092f0e43eeaf688337182486?mode=embed" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" frameborder="0" ';
+var Metal_Chair = 'https://montclair225.autodesk360.com/g/shares/SH35dfcQT936092f0e43da228a37809cc03d?mode=embed" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" frameborder="0" ';
+var Dough_Mixer = 'https://montclair225.autodesk360.com/g/shares/SH35dfcQT936092f0e43e8e02ae73ccad9f9?mode=embed" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" ';
+var Knife_Handle = 'https://montclair225.autodesk360.com/g/shares/SH35dfcQT936092f0e434b0caddc08fb620a?mode=embed" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" ';
+var Drive_Pulley = 'https://montclair225.autodesk360.com/g/shares/SH35dfcQT936092f0e43fd87fc9973054022?mode=embed" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" ';
 
 
 fullscreenContent = document.getElementById("fullscreen");
 
 function openFullscreen(content) {
-  fullscreenContent.innerHTML += content;
+  div = "<iframe style=\"border:none; \" src=' " + content + " \' </iframe>";
+  fullscreenContent.innerHTML += div;
   fullscreenContent.style.display = "block";
 }
+
+// document.getElementById('id').style.pointerEvents = 'auto'; 
 
 function closeFullscreen() {
   fullscreenContent.innerHTML = '<div id="exit" onclick="closeFullscreen()">Exit</div>'
@@ -52,10 +55,10 @@ function showSlides(n) {
 }
 
 // mobilenav
-document.querySelector( ".arrowContainer" ).click(function() {
-  document.querySelector(this).classList.toggle("leftBarAfter");
-});
+// document.querySelector( ".arrowContainer" ).click(function() {
+//   document.querySelector(this).classList.toggle("leftBarAfter");
+// });
 
-document.querySelector( ".arrowContainer" ).click(function() {
-  document.querySelector(this).classList.toggle("rightBarAfter");
-});
+// document.querySelector( ".arrowContainer" ).click(function() {
+//   document.querySelector(this).classList.toggle("rightBarAfter");
+// });
